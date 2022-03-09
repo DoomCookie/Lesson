59c530a8094e7dee47204f23454695539a9bb888
Artem <artem@mail.ru>
2022-03-09 17:43:22 +0300

    09.02.2022

---
+++ Assets/Minecraft/Scripts/MapGenerator.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MapGenerator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] tiles;
+    GameObject[,] map;
+    
+
+    int width, height;
+    // Start is called before the first frame update
+    void Start()
+    {
+        width = 50;
+        height = 50;
+        int[,] map_template = new int[height, width];
+
+        transform.position = new Vector3(-width / 2, 0, -height / 2);
+
+        //int[,] map_template =
+        //{
+        //    {0, 0, 0, 0, 0 },
+        //    {0, 0, 0, 0, 0 },
+        //    {0, 0, 0, 0, 0 },
+        //    {0, 0, 0, 0, 0 },
+        //    {0, 0, 0, 0, 0 }
+        //};
+        //width = map_template.GetUpperBound(1) + 1;
+        //height = map_template.GetUpperBound(0) + 1;
+        map = new GameObject[height, width];
+
+
+        //StreamReader reader = new StreamReader("Assets\\Scripts\\TextFile1.txt");
+        
+        //string[] lines = reader.ReadToEnd().Split('\n');
+        //string[] size = lines[0].Split(' ');
+        //width = int.Parse(size[1]);
+        //height = int.Parse(size[0]);
+        //map_template = new int[height, width];
+        //map = new GameObject[height, width];
+        //for (int i = 0; i < height; ++i)
+        //{
+        //    string[] elems = lines[i + 1].Split(' ');
+        //    width = elems.Length;
+        //    for(int j = 0; j < width; ++j)
+        //    {
+        //        map_template[i, j] = int.Parse(elems[j]);
+        //    }
+        //}
+        
+        int count = 0;
+        for(int i = 0; i < height; ++i)
+        { 
+            for(int j = 0; j < width; ++j)
+            {
+                map[i, j] = Instantiate(tiles[map_template[i, j]]);
+                map[i, j].transform.SetParent(transform);
+                Vector3 pos = new Vector3(i * map[i, j].transform.localScale.x, 0, j * map[i, j].transform.localScale.z);
+                //Vector3 pos = Vector3.right * i * prefab.transform.localScale.x + Vector3.forward * j * prefab.transform.localScale.z;
+                map[i, j].transform.localPosition = pos;
+                count = (count + 1) % tiles.Length;
+            }
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Minecraft/Scripts/Player.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] inventory;
+    GameObject current;
+
+    Ray ray;
+    RaycastHit hit;
+
+    GameObject prepareObject;
+    GameObject lookAtBlock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        if(inventory.Length != 0)
+        {
+            current = inventory[0];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkEvent();
+        ray = new Ray(transform.position, transform.forward);
+        Vector3 newBlockAt;
+        if (Physics.Raycast(ray, out hit, 3))
+        {
+            if (hit.transform.gameObject != prepareObject)
+            {
+                lookAtBlock = hit.transform.gameObject;
+                newBlockAt = hit.transform.position + hit.normal;
+                if (!prepareObject)
+                {
+
+                    prepareObject = Instantiate(current);
+
+                }
+                prepareObject.transform.position = newBlockAt;
+            }
+                
+        }
+        else
+        {
+            Destroy(prepareObject);
+        }
+    }
+
+    void checkEvent()
+    {
+        if (Input.GetButtonDown("Slot1"))
+        {
+            Debug.Log("Now 1");
+            current = inventory[0];
+            Destroy(prepareObject);
+        }
+        else if (Input.GetButtonDown("Slot2"))
+        {
+            Debug.Log("Now 2");
+            current = inventory[1];
+            Destroy(prepareObject);
+        }
+        else if (Input.GetButtonDown("Slot3"))
+        {
+            Debug.Log("Now 3");
+            current = inventory[2];
+            Destroy(prepareObject);
+        }
+        else if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject mapObject = prepareObject;
+            prepareObject = null;
+        }
+        else if (Input.GetButtonDown("Fire2"))
+        {
+            if(lookAtBlock) Destroy(lookAtBlock);
+        }
+    }
+}
